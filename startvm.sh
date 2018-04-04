237c830beb57a6a5cc067435b7668d55d0778751
Connor M <Conmanx360@gmail.com>
2018-04-03 23:50:14 -0400

    Update startvm.sh

--- startvm.sh
+++ startvm.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 sudo qemu-system-x86_64 -enable-kvm -M q35 -m 2G -boot d /home/connor/Virtual\ \Machines/VirtualDebug/linux.qcow2 -device vfio-pci,host=07:00.0 -vga std -smp 8 -trace events=/home/connor/Virtual\ \Machines/events.txt -monitor stdio 
+
+This is my qemu startup script. Nothing special.
