453bcbd78263379b65b7fc662ad887c0fbfa89f7
Connor McAdams <conmanx360@gmail.com>
2018-04-27 11:10:11 -0400

    updated files.
    Signed-off-by: Connor McAdams <conmanx360@gmail.com>

--- QemuHDADump.c
+++ QemuHDADump.c
@@ -38,13 +38,13 @@ unsigned int regionCheck(char *array, unsigned int tlo)
  		printf("char region: %c \n", array[tlo + 40]);
 		return 0;
 	}
-	
+
 }
 
 /*
  * Get the character offset from the PID and the time of the trace.
  */
-int traceLineOffset(char *array) 
+int traceLineOffset(char *array)
 {
 	int i, j;
 	char match_chars[] = { '@', '.', ':', 0 };
@@ -63,13 +63,13 @@ int traceLineOffset(char *array)
  */
 //Should you base it on paranthese ')' instead of newline?
 //Would save you from problems when newline doesn't occur...
-int getLine(char *array) 
+int getLine(char *array)
 {
 	int c;
 	int i = 0;
 
 	memset(&array[0], 0, MAXLINE);
-	while(((i+1) < MAXLINE) && ((c=getchar()) != ')') && (c != '\n') && 
+	while(((i+1) < MAXLINE) && ((c=getchar()) != ')') && (c != '\n') &&
 				(c != '\r') && (c!=EOF)) {
 		array[i] = c;
 		i++;
@@ -85,27 +85,27 @@ int getLine(char *array)
 }
 
 
-void dumpMem(char *array, unsigned short framenumber, int fd) 
+void dumpMem(char *array, unsigned short framenumber, int fd)
 {
 	int i;
 	// int file = fd;
 	unsigned short frameno = framenumber;
 	unsigned int digit_one, digit_two;
-  
+
 	char nl[] = "\n";
 	// char stop[] = "stop\n";
 	// char cont[] = "cont\n";
 	char pmemsave_part1[] = "pmemsave ";
 	char pmemsave_part2[] = " 0x1000 ";
 	char frame[] = "frame";
-	char frameChar[] = "00"; 
-   
+	char frameChar[] = "00";
+
         if (array[0] == '\0')
 	{
-	  printf("dumpMem entered... but the address is not set, skipping\n");   
+	  printf("dumpMem entered... but the address is not set, skipping\n");
 	  return;
         } else
-	  printf("dumpMem entered...\n");   
+	  printf("dumpMem entered...\n");
 
 	for(i = 0; pmemsave_part1[i]; i++) {
 		ioctl(fd, TIOCSTI, pmemsave_part1+i);
@@ -125,20 +125,20 @@ void dumpMem(char *array, unsigned short framenumber, int fd)
 
 	digit_one = (frameno % 10);
 	digit_two = (frameno / 10) % 10;
-	
+
 	frameChar[1] = '0' + digit_one;
 	frameChar[0] = '0' + digit_two;
- 
+
 	for(i = 0; frameChar[i]; i++) {
 		ioctl(fd, TIOCSTI, frameChar+i);
 	}
-   
+
 	ioctl(fd, TIOCSTI, nl);
 
 	return;
 }
 
-int main(int argc, char *argv[]) 
+int main(int argc, char *argv[])
 {
 	char corb_buffer_location[16];
 	char trace_line[MAXLINE];
@@ -147,20 +147,20 @@ int main(int argc, char *argv[])
 	int fd;
 	unsigned int i = 0;
 	unsigned int total_verbs = 0;
-	char cont[] = "cont\n";   
- 
+//	char cont[] = "cont\n";
+
         if (argc <= devno)
 		return 1;
 
 	fd = open(argv[devno], O_RDWR);
-    
+
         if (fd < 0)
 		return 2;
 
-	for(i = 0; cont[i]; i++) {
+/*	for(i = 0; cont[i]; i++) {
 		ioctl(fd, TIOCSTI, cont+i);
 	}
-
+*/
         memset(corb_buffer_location, 0, sizeof(corb_buffer_location));
 
 	while(1) {
@@ -175,7 +175,7 @@ int main(int argc, char *argv[])
 		if (tlo < 0)
 		  	// ignore non-trace lines
 			continue;
-		
+
 		switch_check = regionCheck(trace_line, tlo);
 
 		switch(switch_check) {
@@ -214,7 +214,7 @@ int main(int argc, char *argv[])
 				putchar('\n');
 				break;
 			}
-		
+
 			break;
 		case 2:
 			printf("Current verb 0x%04x Region2+", total_verbs);
@@ -224,9 +224,9 @@ int main(int argc, char *argv[])
 				i++;
 			}
 			putchar('\n');
-		
+
 			break;
-		
+
 		case 3:
 			printf("Current verb 0x%04x Region4+", total_verbs);
 			i = 0;
@@ -235,7 +235,7 @@ int main(int argc, char *argv[])
 				i++;
 			}
 			putchar('\n');
-		
+
 			break;
 		default:
 			printf("switch check %u\n", switch_check);
